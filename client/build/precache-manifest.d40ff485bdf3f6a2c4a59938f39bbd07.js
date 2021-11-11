186e0cf6e3fe21e328f0820074b39431e90b3655
Johnny Dickerson Jr <lilman.jdick@gmail.com>
2021-11-11 00:31:45 -0600

    added mutation js

---
+++ client/build/precache-manifest.d40ff485bdf3f6a2c4a59938f39bbd07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c38709838a3e40b28e61598597b0c41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c2210911768d2b1ca4",
+    "url": "/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "26e22dacb7db05c1520b",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "89c2210911768d2b1ca4",
+    "url": "/static/js/2.62902d5c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.62902d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e22dacb7db05c1520b",
+    "url": "/static/js/main.dd50f128.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -3,10 +3,20 @@ const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +26,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`GraphQL server ready at http://localhost:${PORT}${server.graphqlPath}`);
 });
